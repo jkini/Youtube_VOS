2b2d82bbf86ad26c86d42976de901542caa607e8
jyoti <jyotikini1991@gmail.com>
2019-11-03 03:53:36 -0500

    Added resnet and attention

--- single_object_resnet/config.py
+++ single_object_resnet/config.py
@@ -5,12 +5,13 @@ multi_gpu = False
 
 img_height = 384
 img_width = 384
-batch_size = 3
+batch_size = 2
 n_epochs = 80
-n_frames = 8
+n_frames = 4
 learning_rate = 0.00001
 
-use_trained_weights = False  # to be used at training time
+use_resnet_weights = True # to be used at training time (first training)
+use_trained_weights = False  # to be used at training time (second training onwards)
 train = True 
 
 model_num = 1
@@ -24,7 +25,11 @@ max_vids = 100
 wait_for_data = 5  # in seconds
 batches_until_print = 1
 
-data_dir = '/home/jkini/Datasets/Youtube_VOS_Pretrain/'
+#data_dir = '/home/jkini/Datasets/Youtube_VOS_Pretrain/'
+#data_dir = '/home/jyoti/git/youtube_vos/youtube-vos/'
+#data_dir = '/groups/mshah/data/youtube-vos/'
+data_dir = '/home/jkini/Datasets/Youtube_VOS/'
+resnet_file_name = '../resnet_v1_50.ckpt'
 tf_logs_dir = 'logs/'
 output_inference_dir = './Annotations/'
 output_file_name = 'models/output%d.txt' % model_num
--- single_object_resnet/network.py
+++ single_object_resnet/network.py
@@ -1,55 +1,72 @@
 import config
 import tensorflow as tf
 import sys
-from network_modules import create_vgg_initializer, create_vgg_encoder, create_decoder, create_convlstm
-
+from network_modules import mem_encoder, curr_encoder, attention, curr_decoder
 
 class FullNetwork(object):
     def __init__(self, input_shape=(256, 448)):
         self.graph = tf.Graph()
         self.input_shape = input_shape
         with self.graph.as_default():
-            self.x_vid_input = tf.placeholder(dtype=tf.float32, shape=(None, None, input_shape[0], input_shape[1], 3),
-                                            name='x_vid_input')
-            self.x_seg_input = tf.placeholder(dtype=tf.float32, shape=(None, input_shape[0], input_shape[1], 1),
+            self.x_input = tf.placeholder(dtype=tf.float32, shape=(None, None, input_shape[0], input_shape[1], 3),
+                                            name='x_input')
+            self.x_seg_input = tf.placeholder(dtype=tf.float32, shape=(None, None, input_shape[0], input_shape[1], 1),
                                             name='x_seg_input')
             self.y_input = tf.placeholder(dtype=tf.float32, shape=(None, None, input_shape[0], input_shape[1], 1),
                                         name='y_input')
             self.y_input_mask = tf.placeholder(dtype=tf.float32, shape=(None, None))
             self.init_network()
+            self._variables_to_restore()
             self.init_seg_loss()
             self._summary()
             self.init_optimizer()               
             self.saver = tf.train.Saver()
 
     def init_network(self):
-        c_0, h_0 = create_vgg_initializer(self.x_vid_input[:, 0], self.x_seg_input)
-        x_vid_input_res = self.x_vid_input[:, 1:]
-        x_vid_input_res = tf.reshape(x_vid_input_res,
-                                    (-1, self.input_shape[0], self.input_shape[1], 3))
-        x_t = create_vgg_encoder(x_vid_input_res)
-        _, h, w, ch = x_t.get_shape()
-        h, w, ch = int(h), int(w), int(ch)
-        x_t = tf.reshape(x_t, (config.batch_size, -1, h, w, ch))
-        outputs, state = create_convlstm(c_0, h_0, x_t)
-        _, _, h, w, ch = outputs.get_shape()
-        h, w, ch = int(h), int(w), int(ch)
-        outputs_res = tf.reshape(outputs, (-1, h, w, ch))
-        dec_out = create_decoder(outputs_res)
-        self.segment_layer = tf.reshape(dec_out, (config.batch_size, -1, self.input_shape[0], self.input_shape[1], 1))
-        self.segment_layer_sig = tf.nn.sigmoid(self.segment_layer)
+#         c_0, h_0 = create_vgg_initializer(self.x_vid_input[:, 0], self.x_seg_input)
+#         x_vid_input_res = self.x_vid_input[:, 1:]
+#         x_vid_input_res = tf.reshape(x_vid_input_res,
+#                                     (-1, self.input_shape[0], self.input_shape[1], 3))
+#         x_t = create_vgg_encoder(x_vid_input_res)
+#         _, h, w, ch = x_t.get_shape()
+#         h, w, ch = int(h), int(w), int(ch)
+#         x_t = tf.reshape(x_t, (config.batch_size, -1, h, w, ch))
+#         outputs, state = create_convlstm(c_0, h_0, x_t)
+#         _, _, h, w, ch = outputs.get_shape()
+#         h, w, ch = int(h), int(w), int(ch)
+#         outputs_res = tf.reshape(outputs, (-1, h, w, ch))
+#         dec_out = create_decoder(outputs_res)
+#         self.segment_layer = tf.reshape(dec_out, (config.batch_size, -1, self.input_shape[0], self.input_shape[1], 1))
+#         self.segment_layer_sig = tf.nn.sigmoid(self.segment_layer)
+#         print('Network initialized')
+        
+#         c_0, h_0 = create_vgg_initializer(self.x_vid_input[:, 0, :], self.x_seg_input)
+        
+        # x_t, block1, block2, block3 = create_resnet_curr_encoder(self.x_vid_input[:, 1:, :])
+        # _, t, h, w, ch = x_t.get_shape()
+        # h, w, ch = int(h), int(w), int(ch)
+        # dec_out = create_curr_decoder(x_t, block1, block2, block3)
+
+        mem_key, mem_value, mem_net = mem_encoder(self.x_input[:, :-1, :], self.x_seg_input, is_training=False)
+        curr_key, curr_value, curr_net, block1, block2, block3 = curr_encoder(self.x_input[:, -1:, :], is_training=False)
+        attn_net = attention(mem_key, mem_value, curr_key, curr_value)
+        out_seg = curr_decoder(attn_net, block1, block2, block3)
+
+        self.segment_layer = out_seg
+        self.segment_layer_soft = tf.nn.softmax(self.segment_layer)
         print('Network initialized')
+    
+    def _variables_to_restore(self):
+        exclude = ['mem_encoder/resnet_v1_50/mem', 'curr_encoder/resnet_v1_50/curr', 'attention', 'decoder']
+        self.variables_to_restore = tf.contrib.framework.get_variables_to_restore(exclude=exclude)
 
     def init_seg_loss(self):
         segment = self.segment_layer
-        #y_bbox = self.y_input
         y_input_mask = tf.expand_dims(tf.expand_dims(tf.expand_dims(self.y_input_mask, axis=-1), axis=-1), axis=-1)
-        segmentation_loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=self.y_input[:, 1:, :], logits=segment)
-        segmentation_loss = segmentation_loss * y_input_mask[:, 1:, :]
+        segmentation_loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.y_input[:, -1:, :], logits=segment)
+        segmentation_loss = segmentation_loss * y_input_mask[:, -1:, :]
         segmentation_loss = tf.reduce_mean(tf.reduce_sum(segmentation_loss, axis=[1, 2, 3, 4]))
-        #pred_seg = tf.cast(tf.greater(segment, 0.0), tf.float32)
-        #self.seg_acc = tf.reduce_mean(tf.cast(tf.equal(pred_seg, y_bbox), tf.float32))
-        self.segmentation_loss = segmentation_loss #* config.hr_segment_coef
+        self.segmentation_loss = segmentation_loss
         print('Loss Initialized')
 
     def init_optimizer(self):
@@ -61,9 +78,9 @@ class FullNetwork(object):
     def _summary(self):
         summary = []
         summary.append(tf.summary.scalar('seg_loss', self.segmentation_loss))
-        inp_img = tf.reshape(self.x_vid_input,(-1, self.input_shape[0], self.input_shape[1], 3))
+        inp_img = tf.reshape(self.x_input,(-1, self.input_shape[0], self.input_shape[1], 3))
         summary.append(tf.summary.image('input image', inp_img))
-        pred_mask = tf.reshape(self.segment_layer_sig,(-1, self.input_shape[0], self.input_shape[1], 1))
+        pred_mask = tf.reshape(self.segment_layer_soft,(-1, self.input_shape[0], self.input_shape[1], 1))
         summary.append(tf.summary.image('predicted mask', pred_mask))
         self.summary = tf.summary.merge(summary)
 
@@ -76,7 +93,3 @@ class FullNetwork(object):
         self.saver.restore(sess, file_name)
         print('Model restored.')
         sys.stdout.flush()
-        
-    
-
-#a = FullNetwork()
